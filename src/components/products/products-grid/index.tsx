c574e5c30b6aafb99a5849c1ea4f04efe6bf131e
Evandro Calado <evandrocalado07@gmail.com>
2024-11-06 00:07:20 -0300

    feat: create ProductsGrid component

---
+++ src/components/products/products-grid/index.tsx
@@ -0,0 +1,15 @@
+import { Product } from '@/types';
+import { FC } from 'react';
+import { ProductsGridItem } from '../products-grid-item';
+
+type ProductsProps = {
+  products: Product[];
+};
+
+export const ProductsGrid: FC<ProductsProps> = ({ products }) => (
+  <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+    {products.map((product) => (
+      <ProductsGridItem key={product.slug} product={product} />
+    ))}
+  </div>
+);
