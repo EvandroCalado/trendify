ff241bfdff0e3e942fd80b5e1c0a8371dffcbc27
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 20:03:24 -0300

    style: add cn function

--- src/components/ui/Separator/index.tsx
+++ src/components/ui/Separator/index.tsx
@@ -1,12 +1,14 @@
+import { cn } from '@/utils/cn';
 import { FC } from 'react';
 
 type SeparatorProps = {
   middleText?: string;
+  className?: string;
 };
 
-export const Separator: FC<SeparatorProps> = ({ middleText }) => (
+export const Separator: FC<SeparatorProps> = ({ middleText, className }) => (
   <div className="relative">
-    <div className="my-10 h-px w-full bg-zinc-200" />
+    <div className={cn('my-10 h-px w-full bg-zinc-200', className)} />
     {middleText && (
       <span className="absolute -top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-2 font-semibold">
         {middleText}
