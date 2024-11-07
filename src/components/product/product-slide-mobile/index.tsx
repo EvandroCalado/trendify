7adad687c27ab56d4743f6ac107f22afc9c8aa9d
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 17:36:15 -0300

    feat: create ProductSlideMobile component

---
+++ src/components/product/product-slide-mobile/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Image from 'next/image';
+import { FC } from 'react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+type ProductSlideMobileProps = {
+  images: string[];
+  title: string;
+};
+
+export const ProductSlideMobile: FC<ProductSlideMobileProps> = ({
+  images,
+  title,
+}) => {
+  return (
+    <div className="mb-5 w-full overflow-hidden md:hidden">
+      <Swiper
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2500 }}
+        loop={true}
+        modules={[FreeMode, Autoplay, Pagination]}
+        className="w-full md:rounded-lg"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image} className="w-full bg-white">
+            <Image
+              src={`/products/${image}`}
+              alt={title}
+              width={500}
+              height={500}
+              className="mx-auto object-contain"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
