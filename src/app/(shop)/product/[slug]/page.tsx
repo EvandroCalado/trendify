c993ecfc1380d9456595df66eab1cb93ace97124
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 17:36:58 -0300

    feat: create product by slug page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,64 @@
-const ProductBySlugPage = () => {
+import {
+  ProductQuantity,
+  ProductSizes,
+  ProductSlideDesktop,
+  ProductSlideMobile,
+} from '@/components/product';
+import { initialData } from '@/seeds/seed';
+import { notFound } from 'next/navigation';
+import { FC } from 'react';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const products = initialData.products;
+
+const ProductBySlugPage: FC<Props> = async ({ params }) => {
+  const { slug } = await params;
+
+  const productBySlug = products.find((product) => product.slug === slug);
+
+  if (!productBySlug) notFound();
+
   return (
-    <div>
-      <h1 className="font-title">Product By Slug page</h1>
-    </div>
+    <section className="mx-auto grid w-full max-w-screen-xl grid-cols-1 md:grid-cols-2 md:px-5 lg:gap-16">
+      {/* slide */}
+      <ProductSlideDesktop
+        images={productBySlug.images}
+        title={productBySlug.title}
+      />
+      <ProductSlideMobile
+        images={productBySlug.images}
+        title={productBySlug.title}
+      />
+
+      {/* details */}
+      <div className="w-full space-y-5 px-5">
+        <h1 className="font-title text-xl font-bold">{productBySlug.title}</h1>
+        <p className="text-lg font-semibold">${productBySlug.price}</p>
+
+        {/* sizes */}
+        <ProductSizes
+          availableSizes={productBySlug.sizes}
+          selectedSize={productBySlug.sizes[0]}
+        />
+
+        {/* quantity */}
+        <ProductQuantity quantity={5} />
+
+        {/* button */}
+        <button className="btn-primary px-36 font-semibold">
+          Add to cart
+        </button>
+
+        {/* description */}
+        <h3 className="text-sm font-bold">Description</h3>
+        <p className="text-sm">{productBySlug.description}</p>
+      </div>
+    </section>
   );
 };
 
