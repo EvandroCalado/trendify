ea59582bd807edffab3d2d2702a466e6b596f171
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 20:04:26 -0300

    style: change max width

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -5,7 +5,7 @@ import { initialData } from '@/seeds/seed';
 const products = initialData.products;
 
 const ShopPage = () => (
-  <section className="container mx-auto px-5">
+  <section className="mx-auto w-full max-w-screen-xl px-5">
     <Title title="Shop" subtitle="Discover the latest trends" />
 
     <ProductsGrid products={products} />
