c31b68e6c1275a4eaf9e8033de97b94e932fb185
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 20:03:55 -0300

    feat: create cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,8 +1,83 @@
+import { ProductQuantity } from '@/components/product';
+import { Button, Separator, Title } from '@/components/ui';
+import { initialData } from '@/seeds/seed';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 const CartPage = () => {
   return (
-    <div>
-      <h1 className="font-title">Cart page</h1>
-    </div>
+    <section className="mx-auto w-full max-w-screen-xl gap-5 px-5">
+      <Title title="Cart" />
+
+      <Link href="/" className="text-sm underline underline-offset-4">
+        Continue shopping
+      </Link>
+
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-5 md:gap-16">
+        {/* cart items */}
+        <div className="flex flex-col md:col-span-3">
+          {productsInCart.map((product) => (
+            <div key={product.slug} className="mb-5 flex gap-5 text-sm">
+              <Image
+                src={`/products/${product.images[0]}`}
+                alt={product.title}
+                width={100}
+                height={100}
+                className="rounded max-md:hidden"
+              />
+
+              <div className="flex w-full justify-between">
+                <div className="space-y-2">
+                  <h3 className="font-semibold">{product.title}</h3>
+
+                  <div className="flex items-end gap-5">
+                    <ProductQuantity quantity={3} />
+                    <button className="border-b-[1px] border-zinc-200 transition-all duration-150 hover:border-zinc-900">
+                      Remove
+                    </button>
+                  </div>
+                </div>
+                <div>
+                  <p>${product.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <Separator className="my-5 bg-zinc-300 md:hidden" />
+        </div>
+
+        {/* summary */}
+        <div className="flex h-max flex-col justify-between gap-8 rounded-xl bg-white p-7 shadow-xl md:col-span-2">
+          <h2 className="text-xl font-semibold">Order Summary</h2>
+
+          <div className="flex items-center justify-between text-sm">
+            <span>Shipping</span>
+            <span>Free</span>
+          </div>
+
+          <div className="test-sm flex items-center justify-between">
+            <span>Sales Tax</span>
+            <span>Calculated at checkout</span>
+          </div>
+
+          <div className="flex items-center justify-between text-lg font-semibold">
+            <span>Subtotal</span>
+            <span>$245</span>
+          </div>
+
+          <Button as={'a'} href="/checkout">
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </section>
   );
 };
 
