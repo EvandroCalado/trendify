3e24f1cadc5275b52307f0f7a99c07f7964efd6d
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 00:13:10 -0300

    style: add hight

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,7 @@
 import { Header, Menu } from '@/components/ui';
 
 const ShopLayout = ({ children }: { children: React.ReactNode }) => (
-  <main className="min-h-screen">
+  <main className="flex min-h-screen flex-col">
     <Header />
     <Menu />
     {children}
