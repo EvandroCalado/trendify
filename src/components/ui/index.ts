dd9c226f2c68bc1635b3e192853694fe305788d5
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 22:40:14 -0300

    feat: create Input component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './button';
 export * from './header';
+export * from './input';
 export * from './menu';
 export * from './not-found';
 export * from './Separator';
---
+++ src/components/ui/input/index.tsx
@@ -0,0 +1,52 @@
+import { cn } from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentProps, forwardRef } from 'react';
+
+const inputStyles = cva(
+  [
+    'w-full min-h-11',
+    'bg-zinc-200',
+    'border-[1px] border-zinc-300',
+    'py-2 px-4',
+    'rounded-md',
+    'outline-none',
+    'focus:border-blue-700',
+    'disabled:opacity-40 disabled:pointer-events-none',
+    'transition-all duration-150',
+  ],
+  {
+    variants: {
+      error: {
+        true: 'border-red-500 dark:border-red-500',
+      },
+    },
+    defaultVariants: {},
+  },
+);
+
+type InputProps = ComponentProps<'input'> & {
+  errorMessage?: string;
+} & VariantProps<typeof inputStyles>;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ errorMessage, error, className, ...props }, ref) => {
+    if (errorMessage) {
+      error = !!errorMessage;
+    }
+
+    return (
+      <div>
+        <input
+          ref={ref}
+          {...props}
+          className={cn(inputStyles({ error }), className)}
+        />
+        {errorMessage && (
+          <span className="text-xs text-red-500">{errorMessage}</span>
+        )}
+      </div>
+    );
+  },
+);
+
+Input.displayName = 'Input';
