3816ee2dcc0089b09bc804aa0181bcd7cda4e0e7
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 09:14:04 -0300

    feat: sign in page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,34 @@
+import { Button, Input, Separator, Title } from '@/components/ui';
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1 className="font-title">Login page</h1>
-    </div>
+    <section className="flex flex-1 items-center justify-center px-5">
+      <div className="w-[350px]">
+        <Title title="Login" />
+
+        <label className="text-sm font-semibold">
+          Email
+          <Input type="email" />
+        </label>
+        <label className="mt-5 block text-sm font-semibold">
+          Password
+          <Input type="password" />
+        </label>
+
+        <Button className="mt-5 w-full">Sign in</Button>
+
+        <Separator middleText="or" className="my-5" />
+
+        <Button
+          variant="secondary"
+          as="a"
+          href="/auth/new-account"
+          className="w-full"
+        >
+          Create account
+        </Button>
+      </div>
+    </section>
   );
 };
 
