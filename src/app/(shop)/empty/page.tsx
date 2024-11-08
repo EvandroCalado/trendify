5f6233c1a29b139fd9943cb3fc4313035df1b6d0
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 00:13:20 -0300

    feat: create empty page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const EmptyPage = () => {
   return (
-    <div>
-      <h1 className="font-title">Empty page</h1>
-    </div>
+    <section className="mx-auto flex w-full max-w-screen-xl flex-1 flex-col items-center justify-center gap-5 px-5">
+      <Image src="/img/empty.svg" width={300} height={300} alt="" />
+
+      <h2 className="text-2xl font-semibold">Your cart is empty</h2>
+      <Link
+        href="/"
+        className="text-blue-700 underline underline-offset-4 duration-150 hover:text-blue-800"
+      >
+        Back to shop
+      </Link>
+    </section>
   );
 };
 
