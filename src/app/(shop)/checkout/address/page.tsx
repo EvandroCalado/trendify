ab1e10ecff3a5b5a86e062a91c1dbe0efd1191d4
Evandro Calado <evandrocalado07@gmail.com>
2024-11-07 22:40:39 -0300

    feat: create checkout/address page

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,8 +1,62 @@
+import { Button, Input, Title } from '@/components/ui';
+
 const AddressPage = () => {
   return (
-    <div>
-      <h1 className="font-title">Address page</h1>
-    </div>
+    <section className="mx-auto w-full max-w-screen-xl px-5">
+      <Title title="Address" subtitle="Add your shipping address" />
+
+      <div className="mb-5 flex w-full items-center gap-5 max-md:flex-col">
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Name
+          <Input />
+        </label>
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Nickname
+          <Input />
+        </label>
+      </div>
+
+      <div className="mb-5 flex w-full items-center gap-5 max-md:flex-col">
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Address
+          <Input />
+        </label>
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Address 2 (optional)
+          <Input />
+        </label>
+      </div>
+
+      <div className="mb-5 flex w-full items-center gap-5 max-md:flex-col">
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Zip code
+          <Input />
+        </label>
+        <label className="flex w-full flex-col text-xs font-semibold">
+          City
+          <Input />
+        </label>
+      </div>
+
+      <div className="mb-5 flex w-full items-center gap-5 max-md:flex-col">
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Country
+          <Input />
+        </label>
+        <label className="flex w-full flex-col text-xs font-semibold">
+          Phone
+          <Input />
+        </label>
+      </div>
+
+      <Button
+        as="a"
+        href="/checkout"
+        className="w-max px-32 max-md:w-full md:mt-14"
+      >
+        Continue
+      </Button>
+    </section>
   );
 };
 
