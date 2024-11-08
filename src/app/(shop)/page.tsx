9baf517f2caf2a16dfa33516ac07025ab899d43f
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 20:42:59 -0300

    feat: fetch real data to the database

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,15 +1,17 @@
+import { getPaginatedProducts } from '@/actions/products';
 import { ProductsGrid } from '@/components/products';
 import { Title } from '@/components/ui';
-import { initialData } from '@/seeds';
 
-const products = initialData.products;
+const ShopPage = async () => {
+  const products = await getPaginatedProducts();
 
-const ShopPage = () => (
-  <section className="mx-auto w-full max-w-screen-xl px-5">
-    <Title title="Shop" subtitle="Discover the latest trends" />
+  return (
+    <section className="mx-auto w-full max-w-screen-xl px-5">
+      <Title title="Shop" subtitle="Discover the latest trends" />
 
-    <ProductsGrid products={products} />
-  </section>
-);
+      <ProductsGrid products={products} />
+    </section>
+  );
+};
 
 export default ShopPage;
