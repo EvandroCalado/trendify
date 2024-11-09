b3b5984a1c11596d7b3f0aa56f0f0f5ae1f3c605
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 21:39:16 -0300

    chore: get searchParams for pagination

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,8 +2,16 @@ import { getPaginatedProducts } from '@/actions/products';
 import { ProductsGrid } from '@/components/products';
 import { Title } from '@/components/ui';
 
-const ShopPage = async () => {
-  const products = await getPaginatedProducts();
+type props = {
+  searchParams: {
+    page?: number;
+  };
+};
+
+const ShopPage = async ({ searchParams }: props) => {
+  const { page } = await searchParams;
+
+  const products = await getPaginatedProducts({ page });
 
   return (
     <section className="mx-auto w-full max-w-screen-xl px-5">
