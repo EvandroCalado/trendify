02e727e96ac53c799996e19b9423665cb844e49d
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 21:38:56 -0300

    refactor: receiver page and take dynamic

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -3,9 +3,22 @@
 import { productMapper } from '@/mappers/products';
 import { prisma } from '@/utils/prisma';
 
-export const getPaginatedProducts = async () => {
+type PaginationProps = {
+  page?: number;
+  take?: number;
+};
+
+export const getPaginatedProducts = async ({
+  page = 1,
+  take = 12,
+}: PaginationProps) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
   try {
     const products = await prisma.product.findMany({
+      take,
+      skip: (page - 1) * take,
       include: {
         images: {
           take: 2,
