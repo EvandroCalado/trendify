cfa6514039186a29e3eff16e265969ff4aa75fda
Evandro Calado <evandrocalado07@gmail.com>
2024-11-08 21:47:45 -0300

    feat: redirect to page 1 if page not existis

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,7 @@
 import { getPaginatedProducts } from '@/actions/products';
 import { ProductsGrid } from '@/components/products';
 import { Title } from '@/components/ui';
+import { redirect } from 'next/navigation';
 
 type props = {
   searchParams: {
@@ -12,6 +13,8 @@ const ShopPage = async ({ searchParams }: props) => {
   const { page } = await searchParams;
 
   const products = await getPaginatedProducts({ page });
+
+  if (products.length === 0) return redirect('/');
 
   return (
     <section className="mx-auto w-full max-w-screen-xl px-5">
