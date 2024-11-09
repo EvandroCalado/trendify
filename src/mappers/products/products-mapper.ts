6f33f95ea7049278a252642414996592758c8483
Evandro Calado <evandrocalado07@gmail.com>
2024-11-09 16:42:37 -0300

    chore: type parameter and return

--- src/mappers/products/products-mapper.ts
+++ src/mappers/products/products-mapper.ts
@@ -1,5 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const productMapper = (products: any) => {
+
+import { Gender, Product, Size } from '@/types';
+
+type ProductsDataBase = ({
+  images: {
+    url: string;
+  }[];
+} & {
+  id: string;
+  title: string;
+  description: string;
+  slug: string;
+  price: number;
+  inStock: number;
+  tags: string[];
+  sizes: Size[];
+  gender: Gender;
+  categoryId: string;
+})[];
+
+export const productMapper = (products: ProductsDataBase): Product[] => {
   return products.map((product: any) => ({
     ...product,
     images: product.images.map((image: any) => image.url),
