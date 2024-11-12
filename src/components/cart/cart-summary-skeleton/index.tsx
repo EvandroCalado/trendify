8b70086342d5bbfe268201b66a77550125c879f5
Evandro Calado <evandrocalado07@gmail.com>
2024-11-12 10:38:21 -0300

    feat: create CartSummarySkeleton

---
+++ src/components/cart/cart-summary-skeleton/index.tsx
@@ -0,0 +1,36 @@
+export const CartSummarySkeleton = () => {
+  return (
+    <div className="flex flex-col gap-7">
+      <div className="h-[20px] w-[180px] animate-pulse rounded-lg bg-zinc-200" />
+
+      <div className="flex items-center justify-between">
+        <div className="h-[10px] w-[100px] animate-pulse rounded-lg bg-zinc-200" />
+        <div className="h-[10px] w-[20px] animate-pulse rounded-lg bg-zinc-200" />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center justify-between">
+          <div className="h-[10px] w-[100px] animate-pulse rounded-lg bg-zinc-200" />
+          <div className="h-[10px] w-[60px] animate-pulse rounded-lg bg-zinc-200" />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="h-[10px] w-[120px] animate-pulse rounded-lg bg-zinc-200" />
+          <div className="h-[10px] w-[50px] animate-pulse rounded-lg bg-zinc-200" />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="h-[10px] w-[100px] animate-pulse rounded-lg bg-zinc-200" />
+          <div className="h-[10px] w-[30px] animate-pulse rounded-lg bg-zinc-200" />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="h-[20px] w-[60px] animate-pulse rounded-lg bg-zinc-200" />
+        <div className="h-[20px] w-[60px] animate-pulse rounded-lg bg-zinc-200" />
+      </div>
+
+      <div className="h-[44px] w-full animate-pulse rounded bg-zinc-200" />
+    </div>
+  );
+};
--- src/components/cart/index.ts
+++ src/components/cart/index.ts
@@ -1,2 +1,4 @@
 export * from './cart-products';
 export * from './cart-products-skeleton';
+export * from './cart-summary';
+export * from './cart-summary-skeleton';
