02ff103a6f433f9e877d4c9f449f34d53d1f5fd3
Evandro Calado <evandrocalado07@gmail.com>
2024-11-11 23:55:29 -0300

    reactor: move types to index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,28 @@
-export * from './product';
+export type Size = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
+export type Type = 'shirts' | 'pants' | 'hoodies' | 'hats';
+export type Gender = 'men' | 'women' | 'kid' | 'unisex';
+
+export type Product = {
+  id: string;
+  description: string;
+  images: string[];
+  inStock: number;
+  price: number;
+  sizes: Size[];
+  slug: string;
+  tags: string[];
+  title: string;
+  // type: Type;
+  gender: Gender;
+  categoryId: string;
+};
+
+export type ProductInCart = {
+  id: string;
+  slug: string;
+  title: string;
+  price: number;
+  size: Size;
+  quantity: number;
+  image: string;
+};
