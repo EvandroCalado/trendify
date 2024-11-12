daff432ef3dea07d0c2f87e4be4dc94e27cbd2b6
Evandro Calado <evandrocalado07@gmail.com>
2024-11-12 09:22:11 -0300

    feat: add CartProducts

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,19 +1,10 @@
-import { ProductQuantity } from '@/components/product';
+import { CartProducts } from '@/components/cart';
 import { Button, Separator, Title } from '@/components/ui';
-import { initialData } from '@/seeds';
 import { IconChevronLeft } from '@tabler/icons-react';
-import Image from 'next/image';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
 
 const CartPage = () => {
-  if (productsInCart.length === 0) redirect('/empty');
+  // if (productsInCart.length === 0) redirect('/empty');
 
   return (
     <section className="mx-auto w-full max-w-screen-xl flex-1 gap-5 px-5">
@@ -30,33 +21,7 @@ const CartPage = () => {
       <div className="mt-5 grid grid-cols-1 md:grid-cols-5 md:gap-16">
         {/* cart items */}
         <div className="flex flex-col md:col-span-3">
-          {productsInCart.map((product) => (
-            <div key={product.slug} className="mb-5 flex gap-5 text-sm">
-              <Image
-                src={`/products/${product.images[0]}`}
-                alt={product.title}
-                width={100}
-                height={100}
-                className="rounded max-md:hidden"
-              />
-
-              <div className="flex w-full justify-between">
-                <div className="space-y-2">
-                  <h3 className="font-semibold">{product.title}</h3>
-
-                  <div className="flex items-end gap-5">
-                    <ProductQuantity quantity={3} />
-                    <button className="border-b-[1px] border-zinc-200 transition-all duration-150 hover:border-zinc-900">
-                      Remove
-                    </button>
-                  </div>
-                </div>
-                <div>
-                  <p>${product.price}</p>
-                </div>
-              </div>
-            </div>
-          ))}
+          <CartProducts />
 
           <Separator className="my-5 bg-zinc-300 md:hidden" />
         </div>
