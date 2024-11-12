c0347860fe9c776a367c9d63b5b4bf29a6be1235
Evandro Calado <evandrocalado07@gmail.com>
2024-11-11 23:55:51 -0300

    refactor: ui store to index

--- src/stores/ui/index.ts
+++ src/stores/ui/index.ts
@@ -1,1 +1,11 @@
-export * from './ui-store';
+import { create } from 'zustand';
+
+type MenuState = {
+  isMenuOpen: boolean;
+  toggleMenu: () => void;
+};
+
+export const useUiStore = create<MenuState>()((set) => ({
+  isMenuOpen: false,
+  toggleMenu: () => set((state) => ({ isMenuOpen: !state.isMenuOpen })),
+}));
