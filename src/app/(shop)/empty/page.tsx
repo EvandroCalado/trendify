9f7b1f89b16a18e24f06983b9901a1b75cdbafbd
Evandro Calado <evandrocalado07@gmail.com>
2024-11-12 23:20:30 -0300

    refactor: add Empty component

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,18 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { Empty } from '@/components/ui';
 
 const EmptyPage = () => {
   return (
     <section className="mx-auto flex w-full max-w-screen-xl flex-1 flex-col items-center justify-center gap-5 px-5">
-      <Image src="/img/empty.svg" width={300} height={300} alt="" />
-
-      <h2 className="text-2xl font-semibold">Your cart is empty</h2>
-      <Link
-        href="/"
-        className="text-blue-700 underline underline-offset-4 duration-150 hover:text-blue-800"
-      >
-        Back to shop
-      </Link>
+      <Empty />
     </section>
   );
 };
