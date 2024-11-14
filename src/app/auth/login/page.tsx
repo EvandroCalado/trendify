8a23f9df3d2e5ec103495f3072168466fad96334
Evandro Calado <evandrocalado07@gmail.com>
2024-11-14 09:11:37 -0300

    feat: add Login component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,32 +1,18 @@
-import { Button, Input, Separator, Title } from '@/components/ui';
+import { FormLogin } from '@/components/auth';
+import { Title } from '@/components/ui';
 
 const LoginPage = () => {
   return (
-    <section className="flex flex-1 items-center justify-center px-5">
+    <section className="relative flex flex-1 items-center justify-center px-5">
+      <Title
+        title="Trendify"
+        className="absolute left-5 top-5 font-title capitalize"
+      />
+
       <div className="w-[350px]">
         <Title title="Login" />
 
-        <label className="text-sm font-semibold">
-          Email
-          <Input type="email" />
-        </label>
-        <label className="mt-5 block text-sm font-semibold">
-          Password
-          <Input type="password" />
-        </label>
-
-        <Button className="mt-5 w-full">Sign in</Button>
-
-        <Separator middleText="or" className="my-5" />
-
-        <Button
-          variant="secondary"
-          as="a"
-          href="/auth/new-account"
-          className="w-full"
-        >
-          Create account
-        </Button>
+        <FormLogin />
       </div>
     </section>
   );
